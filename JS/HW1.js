d617d6a1b5b2985949387c6409ab1c3db862d5f9
TricellJS <i.tricell@icloud.com>
2024-03-18 20:31:25 +0200

    HW1

---
+++ JS/HW1.js
@@ -0,0 +1,6 @@
+let number1 = prompt('Введите первое число:');
+let number2 = prompt('Введите второе число:');
+alert(`${Number(number1) + Number(number2)}`); //Number я поставил потому что в prompt считает как строчку.
+alert(`${number1 - number2}`);
+alert(`${number1 * number2}`);
+alert(`${number1 / number2}`);
